91128c62666300c44462da7a41240df0be30c490
Giacomo Longo <gabibbo97@gmail.com>
2020-03-17 20:19:37 +0100

    Aggiunto Moodle versione Docker

---
+++ moodle-docker/install.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+set -e # Fallisci in caso di errore
+
+#
+# Root check
+#
+
+if [ "$(id -u)" != "0" ]; then
+  echo 'Lancia come root' > /dev/stderr && exit 1
+fi
+
+#
+# Docker check
+#
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo "Installare Docker" > /dev/stderr && exit 1
+fi
+
+#
+# Ask for credentials
+#
+
+# Import existing settings
+if [ -f .env ]; then
+  . ./.env
+fi
+
+if [ -z "$DB_PASS" ]; then
+  while [ -z "$DB_PASS" ]; do
+    printf 'Database password? ' && read -r DB_PASS
+  done
+  echo "DB_PASS=$DB_PASS" >> .env
+fi
+
+if [ -z "$MOODLE_USERNAME" ]; then
+  while [ -z "$MOODLE_USERNAME" ]; do
+    printf 'Username di Moodle? ' && read -r MOODLE_USERNAME
+  done
+  echo "MOODLE_USERNAME=$MOODLE_USERNAME" >> .env
+fi
+
+if [ -z "$MOODLE_PASSWORD" ]; then
+  while [ -z "$MOODLE_PASSWORD" ]; do
+    printf 'Password di Moodle? ' && read -r MOODLE_PASSWORD
+  done
+  echo "MOODLE_PASSWORD=$MOODLE_PASSWORD" >> .env
+fi
+
+if [ -z "$MOODLE_EMAIL" ]; then
+  while [ -z "$MOODLE_EMAIL" ]; do
+    printf 'Email di Moodle? ' && read -r MOODLE_EMAIL
+  done
+  echo "MOODLE_EMAIL=$MOODLE_EMAIL" >> .env
+fi
+
+
+if [ -z "$SMTP_HOST" ]; then
+  printf "Desideri configurare l'invio delle email (s/N)? " && read -r MAIL_QUESTION
+  if [ "$MAIL_QUESTION" = "S" ]; then
+
+    if [ -z "$SMTP_HOST" ]; then
+      while [ -z "$SMTP_HOST" ]; do
+        printf 'Indirizzo del server di posta in uscita? ' && read -r SMTP_HOST
+      done
+      echo "SMTP_HOST=$SMTP_HOST" >> .env
+    fi
+
+    if [ -z "$SMTP_PORT" ]; then
+      while [ -z "$SMTP_PORT" ]; do
+        printf 'Porta del server di posta in uscita? ' && read -r SMTP_PORT
+      done
+      echo "SMTP_PORT=$SMTP_PORT" >> .env
+    fi
+
+    if [ -z "$SMTP_USER" ]; then
+      while [ -z "$SMTP_USER" ]; do
+        printf 'Username sul server di posta in uscita? ' && read -r SMTP_USER
+      done
+      echo "SMTP_USER=$SMTP_USER" >> .env
+    fi
+
+    if [ -z "$SMTP_PASSWORD" ]; then
+      while [ -z "$SMTP_PASSWORD" ]; do
+        printf 'Password sul server di posta in uscita? ' && read -r SMTP_PASSWORD
+      done
+      echo "SMTP_PASSWORD=$SMTP_PASSWORD" >> .env
+    fi
+
+    if [ -z "$SMTP_PROTOCOL" ]; then
+      while [ -z "$SMTP_PROTOCOL" ]; do
+        printf 'Protocollo del server di posta (ssl / tls)? ' && read -r SMTP_PROTOCOL
+      done
+      echo "SMTP_PROTOCOL=$SMTP_PROTOCOL" >> .env
+    fi
+
+  fi
+fi
+
+#
+# Setup service
+#
+cat > /etc/systemd/system/moodle.service <<EOF
+[Unit]
+Description = Moodle in Docker
+
+Requires = docker.service
+After = docker.service
+
+[Service]
+ExecStart = $(command -v docker-compose) up
+
+WorkingDirectory = $(pwd)
+
+[Install]
+WantedBy = multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable --now moodle.service
